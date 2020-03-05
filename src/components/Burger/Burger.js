c054e901b6aca77de6623f11aa17fb92b4d83c95
asianvader <phoebe.voong@gmail.com>
2020-03-05 12:46:13 +0000

    add placeholder text when no ingredients have been added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />; // e.g. 'salad', 2
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        console.log(transformedIngredients);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please Start Adding Ingredients</p>
+        }
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"/>
