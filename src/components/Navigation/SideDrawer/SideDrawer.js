5afbd48b1b679ff4eab89777d06f135bcdf19689
asianvader <phoebe.voong@gmail.com>
2020-09-03 14:07:35 +0100

    149: Add backdrop and remove sidedrawer on click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import styles from './SideDrawer.module.css';
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import styles from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
 const sideDrawer = (props) => {
-    return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-            <Logo />
-            </div>
+    let attachedClasses = [styles.SideDrawer, styles.Close];
+    if (props.open) {
+        attachedClasses = [styles.SideDrawer, styles.Open];
+    }
 
-            <nav>
-                <NavigationItems />
-            </nav>
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo />
         </div>
-    );
-}
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
 
 export default sideDrawer;
